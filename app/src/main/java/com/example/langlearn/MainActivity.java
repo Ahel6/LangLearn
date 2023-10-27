4a2dd4279ef246a41d986c4bc5e5de0cbc459e32
Ahel6 <ahellier6@gmail.com>
2023-10-27 13:42:00 +0100

    Cleaned up, working buttons

--- app/src/main/java/com/example/langlearn/MainActivity.java
+++ app/src/main/java/com/example/langlearn/MainActivity.java
@@ -5,20 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{ //implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override //show splash screen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button openSettings = findViewById(R.id.settingsButton);
-        //openSettings.setOnClickListener(this);
     }
 
     public void openSettings(View v){
-        Intent showSettings = new Intent(MainActivity.this, Settings.class);
+        Intent showSettings = new Intent(this, Settings.class);
         startActivity(showSettings);
 
     }
--- app/src/main/java/com/example/langlearn/Settings.java
+++ app/src/main/java/com/example/langlearn/Settings.java
@@ -3,7 +3,6 @@ package com.example.langlearn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Settings extends MainActivity {
 
@@ -12,14 +11,10 @@ public class Settings extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
-    public void openMain() {
-        Button settingsBack = (Button) findViewById(R.id.settingsButton);
-        settingsBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View Main) {
-                startActivity(new Intent(Settings.this, MainActivity.class));
-               //this.finish();
-            }
-        });
+
+    public void openMain(View v){
+        Intent showSettings = new Intent(this, MainActivity.class);
+        startActivity(showSettings);
+
     }
 }
