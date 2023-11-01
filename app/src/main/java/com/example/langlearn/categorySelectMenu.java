72435231130b18c9d4c48c849d1da9bca306b557
Ahel6 <ahellier6@gmail.com>
2023-11-01 15:36:20 +0000

    basics for getExtra

--- app/src/main/java/com/example/langlearn/categorySelectMenu.java
+++ app/src/main/java/com/example/langlearn/categorySelectMenu.java
@@ -1,7 +1,6 @@
 package com.example.langlearn;
 
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +11,7 @@ public class categorySelectMenu extends MainActivity implements View.OnClickList
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_category_select_menu);
 
         Button category1Button = (Button) findViewById(R.id.Category1Button);
@@ -26,25 +26,43 @@ protected void onCreate(Bundle savedInstanceState) {
         Button backButton = (Button) findViewById(R.id.BackButton);
         backButton.setOnClickListener(this);
         }
+        //find which language was chosen
+        Intent Source = this.getIntent();
+        int SelectedLanguage = Source.getIntExtra("LanguageChosen", 0);
 
 @Override
-public void onClick(View view) {
+public void onClick(View view) { //open new activity based on button clicked
         if (view.getId() == R.id.BackButton){
                 Intent backToMain = new Intent(this, MainActivity.class);
                 startActivity(backToMain);
-        /**
-        } else if (view.getId() == R.id.openLanguage1) {
+
+        } else if (view.getId() == R.id.Category1Button) {
                 Intent langMenu = new Intent(this, categorySelectMenu.class);
-        startActivity(langMenu);
-        } else if (view.getId() == R.id.openLanguage2) {
-                Intent langMenu = new Intent(this, categorySelectMenu.class);
+
+                //send the selected language and category
+                langMenu.putExtra("SelectedLanguage", SelectedLanguage);
+                langMenu.putExtra("SelectedCategory",1);
                 startActivity(langMenu);
-        } else if (view.getId() == R.id.openLanguage3) {
+
+        } else if (view.getId() == R.id.Category2Button) {
                 Intent langMenu = new Intent(this, categorySelectMenu.class);
+                langMenu.putExtra("SelectedLanguage", SelectedLanguage);
+                langMenu.putExtra("SelectedCategory",2);
+                startActivity(langMenu);
+
+        } else if (view.getId() == R.id.Category3Button) {
+                Intent langMenu = new Intent(this, categorySelectMenu.class);
+                langMenu.putExtra("SelectedLanguage", SelectedLanguage);
+                langMenu.putExtra("SelectedCategory",3);
+                startActivity(langMenu);
+
+        } else if (view.getId() == R.id.Category4Button) {
+                Intent langMenu = new Intent(this, categorySelectMenu.class);
+                langMenu.putExtra("SelectedLanguage", SelectedLanguage);
+                langMenu.putExtra("SelectedCategory",4);
+
                 startActivity(langMenu);
         }
-         **/
     }
 
-}
 }
