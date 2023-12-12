b8d08e41396ee839cf19775985e0d46725ddb4c1
Ahel6 <ahellier6@gmail.com>
2023-12-12 12:39:17 +0000

    Colour changing on wrong/right answer

--- app/src/main/java/com/example/langlearn/LearningActivity.java
+++ app/src/main/java/com/example/langlearn/LearningActivity.java
@@ -2,17 +2,38 @@ package com.example.langlearn;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
 public class LearningActivity extends MainActivity {
+    public int correctButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learning_activity);
+
+        //element assignment, buttons and text
+        TextView foreignWord = findViewById(R.id.foreignWord);
+
+        Button button1 = findViewById(R.id.LearnButton1);
+        button1.setOnClickListener(this);
+
+        Button button2 = findViewById(R.id.LearnButton2);
+        button2.setOnClickListener(this);
+
+        Button button3 = findViewById(R.id.LearnButton3);
+        button3.setOnClickListener(this);
+
+        Button button4 = findViewById(R.id.LearnButton4);
+        button4.setOnClickListener(this);
+
 
         //get selected language and category
         Bundle Extras = getIntent().getExtras();
@@ -28,17 +49,62 @@ public class LearningActivity extends MainActivity {
 
         Log.println(Log.INFO, "Learning Activity", "Two arrays (hopefully): \n" + langWordsArr + "\n" + engWordsArr); //print the chosen index
 
-        //First creation, set random words on buttons
+        //get a random word from the foreign words array
         Random RandInt = new Random();
         int index = RandInt.nextInt(langWordsArr.size());
         Log.println(Log.INFO, "Marker", "Chosen index " + index); //print the chosen index
-
-        //get a random word from the first (foreign words) array
         String chosen = String.valueOf(langWordsArr.get(index));
         Log.println(Log.INFO, "Marker", "Chosen word " + chosen);
 
+        //set the text based on selected element
+        foreignWord.setText(chosen);
+
+        switch(RandInt.nextInt(4)){//select random button for correct translation
+            case 0:
+                button1.setText(String.valueOf(engWordsArr.get(index)));
+                correctButton = 0;
+                break;
+            case 1:
+                button2.setText(String.valueOf(engWordsArr.get(index)));
+                correctButton = 1;
+                break;
+            case 2:
+                button3.setText(String.valueOf(engWordsArr.get(index)));
+                correctButton = 2;
+                break;
+            case 3:
+                button4.setText(String.valueOf(engWordsArr.get(index)));
+                correctButton = 3;
+                break;
+        }
+
+
+
     }
 
+    public void onClick(View view) { //HORRIBLY inefficient, should probably find a better way of doing this
+        Button button1 = findViewById(R.id.LearnButton1);
+        Button button2 = findViewById(R.id.LearnButton2);
+        Button button3 = findViewById(R.id.LearnButton3);
+        Button button4 = findViewById(R.id.LearnButton4);
+
+        if (view.getId() == R.id.LearnButton1 && correctButton == 0){
+            button1.setBackgroundColor(Color.GREEN);
+        }else if (view.getId() == R.id.LearnButton2 && correctButton == 1){
+            button2.setBackgroundColor(Color.GREEN);
+        }else if (view.getId() == R.id.LearnButton3 && correctButton == 2){
+            button3.setBackgroundColor(Color.GREEN);
+        }else if (view.getId() == R.id.LearnButton4 && correctButton == 3){
+            button4.setBackgroundColor(Color.GREEN);
+        }else{
+            button1.setBackgroundColor(Color.RED);
+            button2.setBackgroundColor(Color.RED);
+            button3.setBackgroundColor(Color.RED);
+            button4.setBackgroundColor(Color.RED);
+
+        }
+
+    }
     public void openMain(View v) {
         Intent showMain = new Intent(this, MainActivity.class);
         startActivity(showMain);
