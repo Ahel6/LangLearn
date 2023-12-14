043d44b060e231e23f903a61e2c61578d725b554
Ahel6 <ahellier6@gmail.com>
2023-12-14 13:43:48 +0000

    Complete rework, no clue if works

--- app/src/main/java/com/example/langlearn/LearningActivity.java
+++ app/src/main/java/com/example/langlearn/LearningActivity.java
@@ -13,11 +13,34 @@ import java.util.ArrayList;
 import java.util.Random;
 public class LearningActivity extends MainActivity {
     public int correctButton;
+
+    private ArrayList langWordsArr;
+    private ArrayList engWordsArr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learning_activity);
 
+        //get selected language and category
+        Bundle Extras = getIntent().getExtras();
+        assert Extras != null;
+        String SelectedLanguage = Extras.getString("LanguageChosen");
+        String SelectedCategory = Extras.getString("SelectedCategory");
+
+        this.setArrays(SelectedLanguage, SelectedCategory);
+
+    }
+
+    public void setArrays (String SelectedLanguage, String SelectedCategory){
+        DatabaseController DBController = new DatabaseController(LearningActivity.this);
+        DBController.getReadableDatabase();
+
+        this.langWordsArr = (DBController.getForeign(SelectedLanguage,SelectedCategory));
+        this.engWordsArr = (DBController.getEnglish(SelectedLanguage,SelectedCategory));
+    }
+
+
+    public void newQuestion(){
         //element assignment, buttons and text
         TextView foreignWord = findViewById(R.id.foreignWord);
 
@@ -33,58 +56,38 @@ public class LearningActivity extends MainActivity {
         Button button4 = findViewById(R.id.LearnButton4);
         button4.setOnClickListener(this);
 
+            //get a random word from the foreign words array
+            Random RandInt = new Random();
+            int index = RandInt.nextInt(langWordsArr.size());
+            Log.println(Log.INFO, "Marker", "Chosen index " + index); //print the chosen index
+            String chosen = String.valueOf(langWordsArr.get(index));
+            Log.println(Log.INFO, "Marker", "Chosen word " + chosen);
 
-        //get selected language and category
-        Bundle Extras = getIntent().getExtras();
-        assert Extras != null;
-        String SelectedLanguage = Extras.getString("LanguageChosen");
-        String SelectedCategory = Extras.getString("SelectedCategory");
-
-        DatabaseController DBController = new DatabaseController(LearningActivity.this);
-        DBController.getReadableDatabase();
-
-        ArrayList<String> langWordsArr = (DBController.getForeign(SelectedLanguage,SelectedCategory));
-        ArrayList<String> engWordsArr = (DBController.getEnglish(SelectedLanguage,SelectedCategory));
-
-        Log.println(Log.INFO, "Learning Activity", "Two arrays (hopefully): \n" + langWordsArr + "\n" + engWordsArr); //print the chosen index
-
-        //get a random word from the foreign words array
-        Random RandInt = new Random();
-        int index = RandInt.nextInt(langWordsArr.size());
-        Log.println(Log.INFO, "Marker", "Chosen index " + index); //print the chosen index
-        String chosen = String.valueOf(langWordsArr.get(index));
-        Log.println(Log.INFO, "Marker", "Chosen word " + chosen);
-
-        //set the text based on selected element
-        foreignWord.setText(chosen);
-
-        switch(RandInt.nextInt(4)){//select random button for correct translation
-            case 0:
-                button1.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton1;
-                break;
-            case 1:
-                button2.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton2;
-                break;
-            case 2:
-                button3.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton3;
-                break;
-            case 3:
-                button4.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton4;
-                break;
+            //set the text based on selected element
+            foreignWord.setText(chosen);
+            switch(RandInt.nextInt(4)){//select random button for correct translation
+                case 0:
+                    button1.setText(String.valueOf(engWordsArr.get(index)));
+                    correctButton = R.id.LearnButton1;
+                    break;
+                case 1:
+                    button2.setText(String.valueOf(engWordsArr.get(index)));
+                    correctButton = R.id.LearnButton2;
+                    break;
+                case 2:
+                    button3.setText(String.valueOf(engWordsArr.get(index)));
+                    correctButton = R.id.LearnButton3;
+                    break;
+                case 3:
+                    button4.setText(String.valueOf(engWordsArr.get(index)));
+                    correctButton = R.id.LearnButton4;
+                    break;
         }
-
-
-
     }
-
     public void onClick(View view) {
         if (view.getId() == correctButton){
             view.setBackgroundColor(Color.GREEN);
-
+            this.newQuestion();
         }else{
             view.setBackgroundColor(Color.RED);
         }
