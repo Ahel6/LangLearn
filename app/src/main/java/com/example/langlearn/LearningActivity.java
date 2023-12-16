d08090a1ced770d782a188bd0b7285df4bd6762c
Ahel6 <ahellier6@gmail.com>
2023-12-16 12:15:46 +0000

    I mean it works and it could be considered complete

--- app/src/main/java/com/example/langlearn/LearningActivity.java
+++ app/src/main/java/com/example/langlearn/LearningActivity.java
@@ -1,27 +1,21 @@
 package com.example.langlearn;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class LearningActivity extends MainActivity {
     public int correctButton;
-
     boolean quesAnswered = false;
     private ArrayList langWordsArr;
     private ArrayList engWordsArr;
-
     private Button button1;
     private Button button2;
     private Button button3;
@@ -29,11 +23,13 @@ public class LearningActivity extends MainActivity {
     private String SelectedLanguage;
     private String SelectedCategory;
 
+    TextView ScoreView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learning_activity);
-
         //get selected language and category
         Bundle Extras = getIntent().getExtras();
         assert Extras != null;
@@ -56,21 +52,23 @@ public class LearningActivity extends MainActivity {
 
     public void setButtons() { //set the listeners and colours of buttons, called when a new question is generated
 
-        this.button1 = findViewById(R.id.LearnButton1);
+        this.button1 = findViewById(R.id.LearnButton0);
         button1.setBackgroundColor(Color.parseColor("#6363E1"));
         button1.setOnClickListener(this);
 
-        this.button2 = findViewById(R.id.LearnButton2);
+        this.button2 = findViewById(R.id.LearnButton1);
         button2.setBackgroundColor(Color.parseColor("#6363E1"));
         button2.setOnClickListener(this);
 
-        this.button3 = findViewById(R.id.LearnButton3);
+        this.button3 = findViewById(R.id.LearnButton2);
         button3.setBackgroundColor(Color.parseColor("#6363E1"));
         button3.setOnClickListener(this);
 
-        this.button4 = findViewById(R.id.LearnButton4);
+        this.button4 = findViewById(R.id.LearnButton3);
         button4.setBackgroundColor(Color.parseColor("#6363E1"));
         button4.setOnClickListener(this);
+
+        this.ScoreView = findViewById(R.id.ScoreNum);
     }
 
 
@@ -96,35 +94,36 @@ public class LearningActivity extends MainActivity {
         switch (RandInt.nextInt(4)) {
             case 0:
                 button1.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton1;
+                correctButton = R.id.LearnButton0;
                 break;
             case 1:
                 button2.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton2;
+                correctButton = R.id.LearnButton1;
                 dontUse = 1;
                 break;
             case 2:
                 button3.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton3;
+                correctButton = R.id.LearnButton2;
                 dontUse = 2;
                 break;
             case 3:
                 button4.setText(String.valueOf(engWordsArr.get(index)));
-                correctButton = R.id.LearnButton4;
+                correctButton = R.id.LearnButton3;
                 dontUse = 3;
                 break;
         }
         engWordsArr.remove(index); //word is removed so it doesn't get used to fill other buttons
+        langWordsArr.remove(index);
 
         //iterate the 4 buttons, adding random words to all of them
-        ViewGroup layout = (ViewGroup) findViewById(R.id.LearningButtons);
-        for (int I = 0; I < layout.getChildCount(); I++) {
+        for (int I = 0; I < 4; I++) {
             if (I != dontUse) {
                 index = RandInt.nextInt(engWordsArr.size());
-                View child = layout.getChildAt(I);
-                Button temp = (Button) child;
+
+                Button temp = (Button) findViewById(getResources().getIdentifier("LearnButton" + I, "id", this.getPackageName()));
                 temp.setText(String.valueOf(engWordsArr.get(index)));
                 engWordsArr.remove(index);
+                langWordsArr.remove(index);
             }
         }
 
@@ -136,6 +135,9 @@ public class LearningActivity extends MainActivity {
             if (view.getId() == correctButton) {
                 view.setBackgroundColor(Color.rgb(49, 173, 65));
                 quesAnswered = true;
+                int Score = Integer.parseInt((String) ScoreView.getText());
+                Score++;
+                ScoreView.setText(String.valueOf(Score));
             } else {
                 view.setBackgroundColor(Color.RED);
             }
